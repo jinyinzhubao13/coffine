afa60af683af2cf6577675ab2a3357d4ea0d7e04
jinyinzhubao13 <xx7q00y@naver.com>
2023-06-09 14:22:23 +0900

    커핀그루나루 2차완성

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(function(){
 
 
+    // 메뉴
     $(".sub").hide();
 
     $(".gnb").mouseenter(function(){
@@ -13,13 +14,36 @@ $(function(){
         $(".header_b").stop().animate({height:"103px"},500)
     }) //mouseleave
 
-
+    // 메인 이미지 슬라이드
     $(".move").slick({
         dots:true,
         arrows:true,
         autoplay:true,
         autoplaySpeed:2000,
     }) //slick
+
+    // 허니브레드
+    $(".left_move").slick({
+        autoplay:true,
+        autoplaySpeed:2500,
+        arrows:false,
+        slidesToScroll:2,
+        dots:true
+    })
+
+    // sns 탭
+    $(".section5_inner li").click(function(e){
+        // 하이퍼링크가 #으로 비어져 있을 때 메인 꼭대기로 빠지는 걸 방지해 줌
+        e.preventDefault()
+        $(".section5_inner li").toggleClass("on")
+    })
+
+    //팝업 배너
+
+    $(".pop").slideDown()
+    $(".pop_inner button").click(function(){
+        $(".pop").slideUp();
+    })
 
 
 
